c334c55d7aeb6b05994f25090d912f2f9edc6466
Insath97 <insath1997.mi@gmail.com>
2025-07-03 12:49:36 +0530

    chnage middleware to admin and merchnat

--- src/routes/admin/amenity.routes.js
+++ src/routes/admin/amenity.routes.js
@@ -4,33 +4,59 @@ const middleware = require("../../middlewares/authMiddleware");
 const validate = require("../../utils/validations/amenity.validation");
 const amenityController = require("../../controllers/admin/amenity.controller");
 
-router.use(middleware.authMiddlewareWithProfile("admin"));
 
 // CRUD Routes
-router.post("/", validate.create, amenityController.createAmenity);
+router.post(
+  "/",
+  middleware.authMiddlewareWithProfile("admin"),
+  validate.create,
+  amenityController.createAmenity
+);
 
-router.get("/", amenityController.getAllAmenities);
+router.get(
+  "/",
+  middleware.authMiddlewareWithProfile(["admin", "merchant"]),
+  amenityController.getAllAmenities
+);
 
-router.get("/:id", validate.getById, amenityController.getAmenityById);
+router.get(
+  "/:id",
+  middleware.authMiddlewareWithProfile("admin"),
+  validate.getById,
+  amenityController.getAmenityById
+);
 
 router.get(
   "/slug/:slug",
+  middleware.authMiddlewareWithProfile("admin"),
   validate.getBySlug,
   amenityController.getAmenityBySlug
 );
 
-router.put("/:id", validate.update, amenityController.updateAmenity);
+router.put(
+  "/:id",
+  middleware.authMiddlewareWithProfile("admin"),
+  validate.update,
+  amenityController.updateAmenity
+);
 
-router.delete("/:id", validate.delete, amenityController.deleteAmenity);
+router.delete(
+  "/:id",
+  middleware.authMiddlewareWithProfile("admin"),
+  validate.delete,
+  amenityController.deleteAmenity
+);
 
 router.patch(
   "/:id/restore",
+  middleware.authMiddlewareWithProfile("admin"),
   validate.restore,
   amenityController.restoreAmenity
 );
 
 router.patch(
   "/:id/toggle-visibility",
+  middleware.authMiddlewareWithProfile("admin"),
   validate.toggleVisibility,
   amenityController.toggleVisibility
 );
